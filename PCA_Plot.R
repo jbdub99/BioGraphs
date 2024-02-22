2ebbcbbc8de0b58859d162e9b7b5f3ec45672195
jweaver <123993888+jbdub99@users.noreply.github.com>
2024-02-22 13:13:50 -0500

    Update PCA_Plot.R

--- PCA_Plot.R
+++ PCA_Plot.R
@@ -1,90 +1,87 @@
-install.packages("ggplot2")
-install.packages("ggfortify")
-install.packages("ggrepel")
-install.packages("ggforce")
 library(ggplot2)
-library(ggfortify)
+library(tidyr)
 library(ggrepel)
-library(ggforce)
 
-#Filter low expressed genes from tmm file
-
-#Set your working directory
-setwd("/your/directory/here)
-
-#Load file with gene counts
-count <- read.csv("RSEM.gene.TMM.EXPR.matrix",sep = "\t",header = T,row.names=1)
+#Load data
+setwd("")
+data <- read.table("",sep = "\t",header = T, row.names=1)
 
 
-#Remove rows if count is < zero in 50% of sample
-rem <- function(x){
-  
-  x <- as.matrix(x)
-  
-  x <- t(apply(x,1,as.numeric))
-  
-  r <- as.numeric(apply(x,1,function(i) sum(i == 0) ))
-  # YOU CAN CHANGE THIS TO A HIGHER VALUE
-  remove <- which(r > dim(x)[2]*0.5)
-  
-  return(remove)
-  
-}
-
-remove <- rem(count)
-
-countdata <- count[-remove,]
-
-write.table(countdata, "remove100.txt")
+#Remove rows if gene expression is less than .5 in 50% of samples
+rem <- data[apply(data, MARGIN = 1, function(x) sum(x<=1)) < ncol(data)/2,]
 
 
-#Store your TMM expression file in the data object, storing the first row as column headings, and first column as row names
-data <- read.table("remove100.txt", head=TRUE, row.names=1)
+pruned_data <- data[  row.names(data) %in% row.names(rem), ]
 
-#Transform data to proper orientation for PCA analysis
-data_t <- t(data)
+head(pruned_data)
 
-#Store rownames as an object 
-rows <- c(rownames(data_t))
-
-#Pca analysis
-data.pca <- prcomp(data_t, center = TRUE, scale.=TRUE)
-
-#Take a peek and get PC1 and PC2 % values for the axes -- probably a way to do this with code to save them for use as xlab / ylab
-
-pcs <- summary(data.pca)
-pcone <- sd(pcs$x[, 1])
-pctwo <- sd(pcs$x[, 2])
-
-#Enter your sample group names and how many replicates there are in each group
-samples <- c(
-  rep("A_1", 3), rep("A_2", 3), rep("A_3", 3),
-  rep("B_1", 3), rep("B_2", 3), rep("B_1", 3),
-)
+dim(pruned_data)
 
 
-
-#Create groups for your samples to be used in the PCA graph
-A <- samples(C(1,9))
-B <- samples(c(10,18))
+#center columns so samples are centered on same mean
+scaled1 <- scale(pruned_data)
 
 
-# Convert PCA output into a data frame
-data.plottable <- data.pca$x
-  
-)
+#transform to be able to center so genes have same variance
+t_scaled1 <- t(scaled1)
 
-# Plot PC1 vs PC2, coloring and shaping based on Plant_Genotype
-library(ggplot2)
 
-pdf("PCA_Plot.pdf", width = 6.5, height = 4)
-ggplot(data.plottable, aes(x = PC1, y = PC2, shape = Group, color = Group)) +
-  geom_point(size = 5) +
-  xlab(pcone) +
-  ylab(pctwo) +
-  labs(title = " ") +
-  theme(plot.title = element_text(size = 50, hjust = 0.5)) +
-  theme_classic()
+#center on genes
+scaled2 <- scale(t_scaled1)
+
+
+#capture sample names
+rows <- c(rownames(scaled2)) %>% as.data.frame()
+
+
+#perform principle components analysis to get values
+datapca <- prcomp(scaled2, center=TRUE, scale.=TRUE)
+
+
+#look at the first 2 pc values to see the % variance they explain
+#Store the values of PC1 and PC2 for graph axes
+summary(datapca)
+pc1 <- round(summary(datapca)$importance[2,1]*100,digits=2)
+pc2 <- round(summary(datapca)$importance[2,2]*100,digits=2)
+pc1
+pc2
+
+#grab the first two principle components values
+data <- as.data.frame(datapca$x)
+data <- data[,1:2]
+
+
+#separate sample names into a table with each information in a new column
+test <- separate(rows, ., into=c("A","B","C"))
+
+
+#combine the separated sample names with the PCA data
+test1 <- cbind(test, data)
+
+head(test1)
+
+
+##This following section of splitting by Chill hours is still a work in progress
+##I know I can do it dynamically instead of manually, just haven't learned yet
+
+#separate the data into subsets based on chill hours
+data_subset <- split(test1, test1$Fungi, drop=FALSE)
+
+summary(data_subset)
+
+#assign each subset to its own dataframe, Change 'X' to be the seperator of column names
+df <- data_subset$"x" %>% as.data.frame()
+
+#Plot the PCA plots
+
+pdf(file = "")
+ggplot(fung, aes(x=PC1, y=PC2)) +
+  geom_point(aes(colour=Genotype, shape=Timepoint, size=5)) +
+  #geom_text_repel(aes(label=row.names(Chill400)), size = 3, box.padding = .25, point.padding = .5, max.overlaps = 50) +
+  xlab(pc1) +
+  ylab(pc2) +
+  ggtitle("") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  guides(size="none")
 dev.off()
-
 
